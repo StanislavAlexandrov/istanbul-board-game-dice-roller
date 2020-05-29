c004488efae202f0304926e8ecbcba0ddf92b1d6
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2020-05-29 12:34:44 +0500

    switch png to webp

--- js/index.js
+++ js/index.js
@@ -13,17 +13,17 @@ const allTiles = [
 ];
 
 const allTilesDict = {
-    "2 - Fabric Warehouse": "img/2-fabric-warehouse.png",
-    "3 - Spice Warehouse": "img/3-spice-warehouse.png",
-    "4 - Fruit Warehouse": "img/4-fruit-warehouse.png",
-    "5 - Post Office": "img/5-post-office.png",
-    "6 - Caravansary": "img/6-caravansary.png",
-    "7 - Fountain": "img/7-fountain.png",
-    "8 - Black Market": "img/8-black-market.png",
-    "9 - Teahouse": "img/9-teahouse.png",
-    "10 - Large Market": "img/10-large-market.png",
-    "11 - Small Market": "img/11-small-market.png",
-    "12 - Police Station": "img/12-police-station.png",
+    "2 - Fabric Warehouse": "img/2-fabric-warehouse.webp",
+    "3 - Spice Warehouse": "img/3-spice-warehouse.webp",
+    "4 - Fruit Warehouse": "img/4-fruit-warehouse.webp",
+    "5 - Post Office": "img/5-post-office.webp",
+    "6 - Caravansary": "img/6-caravansary.webp",
+    "7 - Fountain": "img/7-fountain.webp",
+    "8 - Black Market": "img/8-black-market.webp",
+    "9 - Teahouse": "img/9-teahouse.webp",
+    "10 - Large Market": "img/10-large-market.webp",
+    "11 - Small Market": "img/11-small-market.webp",
+    "12 - Police Station": "img/12-police-station.webp",
 };
 
 function myFunction() {
