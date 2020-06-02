aa33c1ec427ec4231523bae82fcdbf8d52135186
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2020-06-02 10:09:03 +0500

    add fallback option for images in safari

--- js/index.js
+++ js/index.js
@@ -26,11 +26,32 @@ const allTilesDict = {
     "12 - Police Station": "img/12-police-station.webp",
 };
 
+const allTilesDictSafari = {
+    "2 - Fabric Warehouse": "img/2-fabric-warehouse.jpg",
+    "3 - Spice Warehouse": "img/3-spice-warehouse.jpg",
+    "4 - Fruit Warehouse": "img/4-fruit-warehouse.jpg",
+    "5 - Post Office": "img/5-post-office.jpg",
+    "6 - Caravansary": "img/6-caravansary.jpg",
+    "7 - Fountain": "img/7-fountain.jpg",
+    "8 - Black Market": "img/8-black-market.jpg",
+    "9 - Teahouse": "img/9-teahouse.jpg",
+    "10 - Large Market": "img/10-large-market.jpg",
+    "11 - Small Market": "img/11-small-market.jpg",
+    "12 - Police Station": "img/12-police-station.jpg",
+};
+
 function myFunction() {
     resultingTile = randomFrom(allTiles);
     document.querySelector("#result").innerHTML = resultingTile;
-    console.log(allTilesDict[resultingTile]);
-    document.querySelector("#tile-picture").src = allTilesDict[resultingTile];
+    let iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    console.log(iOS);
+    if (iOS) {
+        document.querySelector("#tile-picture").srcset =
+            allTilesDictSafari[resultingTile];
+    } else {
+        document.querySelector("#tile-picture").srcset =
+            allTilesDict[resultingTile];
+    }
 }
 
 function randomFrom(array) {
